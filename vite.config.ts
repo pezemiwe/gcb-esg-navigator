228d215be6dcd3ca72db85c413b8fd86c10660ad
pc-eze <pezemiwe@deloitte.com.ng>
2026-02-06 06:47:45 +0100

    quick-fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/gcb-esg-navigator/",
   plugins: [react()],
   resolve: {
     alias: {
